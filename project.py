6b701e995923b092603dee0374eb35cfb5009800
Your NameKelly <kellyamacl@gmail.com>
2017-12-17 16:23:08 -0500

    text is changed to an array

--- project.py
+++ project.py
@@ -14,17 +14,26 @@ trainX, trainY = downloadData.getData()
 
 
 vectorizer = CountVectorizer()
-print( vectorizer.fit_transform(trainX).todense() )
-print( vectorizer.vocabulary_ )
+vectorizer.fit_transform(trainX).todense() 
+#print( vectorizer.vocabulary_ )
 print(len(vectorizer.vocabulary_))
 
-vectorizedTrainX = vectorizer.transform(trainX)
+vectorizedTrainX = []
+arrayX = []
+for x in trainX:
+	transformed = []
+	for word in x.split():
+		transformed.append(vectorizer.vocabulary_.get(word))
+	vectorizedTrainX.append(transformed)
+
 print(trainX[1])
 print(vectorizedTrainX[1])
 print(trainY[1])
 trainX = vectorizedTrainX
+print( vectorizer.vocabulary_.get("great") )
+print( vectorizer.vocabulary_.get("phone") )
 
-
-
-
-
+# Great phone!.
+# r
+#   (0, 2023)	1
+#   (0, 3322)
